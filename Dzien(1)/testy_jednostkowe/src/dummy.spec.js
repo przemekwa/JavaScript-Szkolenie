abedbd21a275dc03d02810603e64c1ff081e7988
przemekwa <przemyslaw.walkowski@bzwbk.pl>
2016-09-28 14:44:25 +0200

    Dzialające testy

---
+++ Dzien(1)/testy_jednostkowe/src/dummy.spec.js
@@ -0,0 +1,15 @@
+/**
+ * Created by 127356 on 2016-09-28.
+ */
+
+
+describe('Dummy', function(){
+    it('runs this test', function () {
+        expect(1+5).not.toBe(6);
+   });
+
+    it('efies dummy data'), function () {
+        expect(dummy).not.toBeDefined();
+    }
+});
+
