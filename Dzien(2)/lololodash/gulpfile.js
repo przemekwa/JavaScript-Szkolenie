0100affb50b227ad96b7f12a3e9077bb1b2af2cd
przemekwa <przemyslaw.walkowski@bzwbk.pl>
2016-09-29 12:00:34 +0200

    Gulp - Jade - Pug - przykłady

--- Dzien(2)/lololodash/gulpfile.js
+++ Dzien(2)/lololodash/gulpfile.js
@@ -1,11 +1,25 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var pug = require('gulp-pug');
+
+gulp.task('views', function buildHTML() {
+ return gulp.src('./*.pug')
+     .pipe(pug({locals: {name: "Timothy"} }
+         ))
+     .pipe(gulp.dest("./html"));
+});
+
 
 gulp.task('styles', function () {
 
  gulp.src("./main.scss").pipe(sass()).pipe(gulp.dest("./dist"));
 
 });
+
+gulp.task('default',["styles", "views"], function () {
+
+ gulp.watch('./*.scss', ['styles'])
+})
 
 gulp.task('hello', function () {
  console.log("Hello World!")
