34a39763fe94bf48b26bd17d234701efefea18b1
przemekwa <przemyslaw.walkowski@bzwbk.pl>
2016-09-30 11:27:35 +0200

    TypeScript - generyki

--- Dzien(3)/src/library.js
+++ Dzien(3)/src/library.js
@@ -7,6 +7,23 @@ var Book = (function () {
     }
     return Book;
 }());
-var book1 = new Book('The Bible', 'The Good', 96);
-var book2 = new Book('JavaScript', 'Douglas', 2005);
+return true;
+;
+var book1 = new Book('The Bible', { name: 'GOD', lastName: 'GOD' }, 96);
+var book2 = new Book('JavaScript', { name: 'Doglas', lastName: 'John' }, 2005);
+var booksList;
+var userIds;
+booksList = adder(booksList, book1);
+booksList = adder(booksList, book2);
+booksList = adder(booksList, {
+    id: "sdas",
+    name: 'ABS',
+    author: { name: 'A', lastName: "B" },
+    year: 23,
+    read: 2
+});
+userIds: adder(userIds, 2);
+function adder(list, book) {
+    return list.concat([T]);
+}
 //# sourceMappingURL=library.js.map
--- Dzien(3)/src/library.ts
+++ Dzien(3)/src/library.ts
@@ -1,14 +1,49 @@
-class Book {
-    constructor(public name: string,
-                public author: string,
-                public year: number,
-                public read?: number) {
-
-
-    }
+interface IAuthor {
+    name: string,
+    lastName: string,
+    birthDate?: number
 }
 
-const book1 = new Book('The Bible', 'The Good', 96);
-const book2 = new Book('JavaScript', 'Douglas', 2005);
+interface ILendable {
+    id: string,
+    borrow: (userId: string)=> boolean
+}
+
+class Book implements ILendable{
+    constructor(public name: string,
+                public author: IAuthor,
+                public year: number,
+                public read?: number) {
+    }
+    id: string;
+    borrow?: (userId: string)=>{
+        return true;
+    };
+}
+
+const book1 = new Book('The Bible', {name: 'GOD', lastName: 'GOD'}, 96);
+const book2 = new Book('JavaScript', {name: 'Doglas', lastName: 'John'}, 2005);
+
+let booksList: Book[];
+let userIds: number[];
+
+booksList = adder(booksList, book1);
+booksList = adder(booksList, book2);
+booksList = adder(booksList, {
+    id: "sdas",
+    name: 'ABS',
+    author: {name: 'A', lastName: "B"},
+    year: 23,
+    read: 2
+});
+
+
+userIds: adder(userIds, 2);
+
+
+function adder<T>(list: T[], book: T): T[]{
+    return [...list, T];
+}
+
 
 
